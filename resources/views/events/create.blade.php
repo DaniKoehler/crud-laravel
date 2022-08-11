b63d4541ea77daeade445bc25069ca06aefe2981
Daniel Koehler <ntckoehler@gmail.com>
2022-08-11 13:34:20 -0400

    pag lista eventos

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -6,26 +6,31 @@
 
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Criar Evento</h1>
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
             <div id="event-create-form" class="form-group">
-                <label for="title">Evento:</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+                <label for="image">Imagem do Evento</label>
+                <input type="file" class="form-control-file" id="image" name="image" placeholder="Nome do evento" required>
             </div>
             <div id="event-create-form" class="form-group">
-                <label for="title">Cidade:</label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+                <label for="title">Evento*</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento" required>
             </div>
             <div id="event-create-form" class="form-group">
-                <label for="title">O evento é privado?</label>
+                <label for="title">Cidade*</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento" required>
+            </div>
+            <div id="event-create-form" class="form-group">
+                <label for="title">O evento é privado?*</label>
                 <select name="private" id="private" class="form-control">
                     <option value="0">Não</option>
                     <option value="1">Sim</option>
                 </select>
             </div>
             <div id="event-create-form" class="form-group">
-                <label for="title">Descrição:</label>
-                <textarea name="description" id="description" placeholder="O que vai acontecer no evento?" class="form-control"></textarea>
+                <label for="title">Descrição*</label>
+                <textarea name="description" id="description" placeholder="O que vai acontecer no evento?"
+                class="form-control" required></textarea>
             </div>
             <input id="event-create-form" type="submit" class="btn btn-primary" value="Criar Evento">
         </form>
