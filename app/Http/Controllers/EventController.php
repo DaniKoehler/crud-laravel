c35cad03c2513fca8f8a8a77884beab85bc4538a
Daniel Koehler <ntckoehler@gmail.com>
2022-08-12 16:13:30 -0400

    conclusao crud

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -57,28 +57,49 @@ class EventController extends Controller
     }
 
     public function show($id) {
-
         $event = Event::findOrFail($id);
-
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
-
         return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 
     public function dashboard() {
-
         $user = auth()->user();
-
         $events = $user->events;
-
         return view('events.dashboard', ['events' => $events]);
     }
 
     public function destroy($id) {
-
         Event::findOrFail($id)->delete();
-
         return redirect('/dashboard')->with('msg', 'Evento deletado com sucesso!');
+    }
+
+    public function edit($id) {
+        $event = Event::findOrFail($id);
+        return view('events.edit', ['event' => $event]);
+    }
+
+    public function update(Request $request) {
+        $data = $request->all();
+
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+            $extension = $requestImage->extension();
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . '.' . $extension;
+            $requestImage->move(public_path('img/events'), $imageName);
+            $data['image'] = $imageName;
+
+        }
+
+        Event::findOrFail($request->id)->update($data);
+        return redirect('/dashboard')->with('msg', 'Evento atualizado com sucesso!');
+    }
+
+    public function joinEvent($id) {
+        $user = auth()->user();
+        $user->eventsAsParticipants()->attach($id);
+        $event = Event::findOrFail($id);
+        return redirect('/dashboard')->with('msg', 'Você se inscreveu no evento ' . $event->title . '!');
     }
 
 }
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -12,7 +12,8 @@
             <div id="info-container" class="col-md-6">
                 <h1>{{ $event->title }}</h1>
                 <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{ $event->city }}</p>
-                <p class="events-participants"><ion-icon name="people-outline"></ion-icon> X Participantes</p>
+                <p class="events-participants"><ion-icon name="people-outline"></ion-icon>
+                    {{ count($event->users) }} Participantes</p>
                 <p class="event-owner"><ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }}</p>
                 <h3>Detalhes do Evento</h3>
                 <ul id="items-list">
@@ -21,7 +22,12 @@
                     @endforeach
                     <p class="event-description">Data: {{ date('d/m/Y'), strtotime($event->date) }}</p>
                 </ul>
-                <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+                <form action="/events/join/{{ $event->id }}" method="POST">
+                    @csrf
+                    <a href="/events/join/{{ $event->id }}" class="btn btn-primary"
+                        id="event-submit" onclick="event.preventDefault();
+                        this.closest('form').submit();">Confirmar Presença</a>
+                </form>
             </div>
             <div class="col-md-12" id="description-container">
                 <h3>Sobre o Evento:</h3>
